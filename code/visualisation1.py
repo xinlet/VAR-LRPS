90dd15738ef1d96ac656282925082552c1ec541e
Xinle Tian <72056199+xinlet@users.noreply.github.com>
2026-01-14 21:56:04 +0000

    Update visualisation1.py

--- code/visualisation1.py
+++ code/visualisation1.py
@@ -3,14 +3,11 @@ import glob
 import numpy as np
 import pandas as pd
 
-# Directory containing your estimate CSVs
 csv_dir = 'your directory'
 
-# Find all CSVs
 lrps_files = glob.glob(os.path.join(csv_dir, '*LRPS*.csv')) #you can change this to switch between method
 print(f"Found {len(lrps_files)} LRPS files.")
 
-# Read all as numpy arrays, store in a list
 matrices = [pd.read_csv(f, header=0).to_numpy() for f in lrps_files]
 
 # Check all have the same shape
@@ -26,15 +23,13 @@ from nilearn import plotting, datasets
 import copy
 import pandas as pd
 
-# Fetch AAL atlas and coordinates for first 113 regions
 atlas = datasets.fetch_atlas_aal()
 atlas_filename = atlas['maps']
 coords = plotting.find_parcellation_cut_coords(atlas_filename)[:116]
 
-# Set a percentile threshold (e.g., keep top 1% of all values)
+# Keep top 1%
 percentile = 99  # keep connections above 99th percentile
 
-# Work with absolute values for GC strength
 G_abs = np.abs(G)
 thresh = np.percentile(G_abs, percentile)
 G_threshold = np.copy(G)
@@ -42,7 +37,6 @@ G_threshold[G_abs < thresh] = 0
 
 print(f"Thresholded at {thresh:.4f}; number of connections kept: {np.count_nonzero(G_threshold)}")
 
-# Plot
 plotting.plot_connectome(
     G_threshold,
     coords,
